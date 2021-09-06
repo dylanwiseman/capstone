a8e7cbb09b979f855d5b9d92b53938c7119ae0ed
Dylan Wiseman <dylan.wiseman22@gmail.com>
2021-09-06 11:54:09 -0600

    my edit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [userData, setUserData] = useState({});
+
+  useEffect(() => {
+    async function getUser() {
+      const { data } = await axios.get("/api");
+      setUserData(data);
+    }
+    try {
+      getUser();
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+
+  return <div className="App">{userData.username}</div>;
 }
 
 export default App;
